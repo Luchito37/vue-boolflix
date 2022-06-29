e67609663d53c8c48c5583a8b6a05aa321a9ae08
Luchito37 <lucamatteotti01@gmail.com>
2022-06-29 09:48:13 +0200

    corretto un punto visivo

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -3,8 +3,8 @@
         <div class="container">
             <div id="movieList" class="row">
                 <div class="col">
-                    <ul class="list-unstyled">
-                        <li v-for=" (movie) in state.listaFilm" :key="movie">
+                    <ul class="list-unstyled d-flex flex-wrap justify-content-center mt-4">
+                        <li class="mx-3" v-for=" (movie) in state.listaFilm" :key="movie">
                             <filmCard :movie="movie"></filmCard>
                         </li>
                     </ul>
